9b642b531cf90d12feeb67db13fe1c50a50264ab
GabrielBergamim <gabriel.bergamim@outlook.com>
2022-02-28 17:47:40 -0300

    Register page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Register } from './pages/Register';
+
+import styles from './App.module.css';
+
+export function App() {
+
+  return (
+    <main className={styles.app}>
+      <Register />
+    </main>
+  );
+}
